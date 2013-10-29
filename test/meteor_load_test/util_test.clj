2b8740e824d310ac963ec4a5e8b1687014ea909c
alanning <alanning@gmail.com>
2013-10-29 12:20:26 -0400

    tidy formatting

--- test/meteor_load_test/util_test.clj
+++ test/meteor_load_test/util_test.clj
@@ -13,8 +13,7 @@
           expected2 "[\"addEntry\", {\"addEntry\":[{\"ownerId\":\"0-156.122.171.108.client.dyn.strong-in125.as13926.net-16-0\",\"name\":\"load-5\",\"type\":\"client\"}]}]"
       ]
       (is (= expected1 (replace-words s m)))
-      (is (= expected2 (replace-words s2 m2)))
-      )))
+      (is (= expected2 (replace-words s2 m2))))))
 
 (deftest test-ensure-uri-string
   (testing "ensure-uri with string param"
